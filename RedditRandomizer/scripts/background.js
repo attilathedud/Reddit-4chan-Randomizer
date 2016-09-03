8467a1fc03573317243c3ed6ba67b2725372cb95
Nathan Tucker <abezhat@verizon.net>
2016-09-03 19:33:14 +0930

    Adding randomizer for 4chan

--- RedditRandomizer/scripts/background.js
+++ RedditRandomizer/scripts/background.js
@@ -5,6 +5,20 @@ var bShowLoadingDialog          = true;
 var bIsActivated                = true;
 var bIncludeNsfwResults         = false;
 
+var sfw_boards                  = [ 'a', 'c', 'w', 'm', 'cgl', 'cm', 'f', 'n', 'jp', 'v', 'vg', 'vp', 'vr',
+                                    'co', 'g', 'tv', 'k', 'o', 'an', 'tg', 'sp', 'asp', 'sci', 'his', 'int',
+                                    'out', 'toy', 'i', 'po', 'p', 'ck', 'ic', 'wg', 'mu', 'fa', '3', 'gd', 'diy',
+                                    'wsg', 'qst', 'biz', 'trv', 'fit', 'x', 'lit', 'adv', 'lgbt', 'mlp', 'news',
+                                    'wsr'];
+
+var nsfw_boards                 = [ 'b', 'r9k', 'pol', 'soc', 's4s', 's', 'hc', 'hm', 'h', 'e', 'u', 'd', 'y', 
+                                    't', 'hr', 'gif', 'aco', 'r'];
+
+
+function get_random_index( a ) {
+    return a[ Math.floor( Math.random() * a.length ) ];
+}
+
 function init_handler( ) {
     var hotkey_jwerty_combo     = '';
 
@@ -25,11 +39,27 @@ function init_handler( ) {
             });
         }
 
-        if( bIncludeNsfwResults && Math.random() < 0.5 ) {
-            window.location = 'https://www.reddit.com/r/randnsfw';
+        if( window.location.href.includes(".4chan.org") ) {
+            var selected_board  = '';
+            var selected_thread = '';
+
+            var possible_boards = sfw_boards;
+            if( bIncludeNsfwResults && Math.random() < 0.5 ) {
+                possible_boards = sfw_boards.concat( nsfw_boards );
+            }
+
+            selected_board = get_random_index( possible_boards );
+
+            $.getJSON( "https://a.4cdn.org/" + selected_board + "/threads.json", function( data ) {
+                var selected_page = get_random_index( data );
+                selected_thread   = get_random_index( selected_page.threads ).no;
+                
+                window.location = 'https://boards.4chan.org/' + selected_board + '/thread/' + selected_thread;
+            });
         }
         else {
-            window.location = 'https://www.reddit.com/r/random';
+            window.location = 'https://www.reddit.com/r/' + 
+                ( ( bIncludeNsfwResults && Math.random() < 0.5 ) ? 'randnsfw' : 'random' );
         }
     });
 }
